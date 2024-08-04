e8e407743490b03ab41abe5b7482dcce5f675ac6
aekmekci72 <anna.ekmekci@gmail.com>
2024-08-04 10:58:28 -0400

    transferring to render

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,12 +1,12 @@
-// config/firebaseConfig.js
-const admin = require('firebase-admin');
-const serviceAccount = require('../serviceKey.json'); // Replace with your Firebase service account key file
+// // config/firebaseConfig.js
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../serviceKey.json'); // Replace with your Firebase service account key file
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<your-database-name>.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://<your-database-name>.firebaseio.com"
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-module.exports = db;
+// module.exports = db;
